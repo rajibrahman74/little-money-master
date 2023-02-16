1173ef8ea37ffc584b2fdb89a1db68b5f442637f
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-16 16:26:51 +0600

    [ADD]-finished-savings

--- js/expenses.js
+++ js/expenses.js
@@ -101,11 +101,78 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const mainBalanceString = mainBalance.innerText;
   const mainBalanceValue = parseInt(mainBalanceString);
 
-  //get main balance 
 
+  //get main balance 
 
   const totalMianBalance = mainBalanceValue + getMainBalance;
 
   mainBalance.innerText = totalMianBalance;
 
 });
+
+
+
+
+
+// Add event listener in the calculate button
+
+document.getElementById('save-btn').addEventListener('click', function () {
+
+      // get Income field
+  const incomeField = document.getElementById("income-field");
+  const incomeFieldString = incomeField.value;
+  const incomeFieldValue = parseInt(incomeFieldString);
+
+  // get saving%
+
+  const savingField = document.getElementById('saving-field');
+  const savingFieldString = savingField.value;
+  const savingFieldValue = parseFloat (savingFieldString);
+
+
+  // convert savings
+
+  const savings = incomeFieldValue * (savingFieldValue / 100);
+
+
+  // get saving total element
+  
+  const totalSaving = document.getElementById('total-savings');
+  const totalSavingString = totalSaving.innerText;
+  const totalSavingValue = parseInt (totalSavingString);
+
+  const totalSavingsInIncome = savings + totalSavingValue;
+
+  totalSaving.innerText = totalSavingsInIncome;  // Total saving
+
+
+
+
+
+  // Set Remaining Balance
+
+
+  // get main balance for set remaining balance
+
+  const mainBalance = document.getElementById('main-balance');
+  const mainBalanceString = mainBalance.innerText;
+  const mainBalanceValue = parseInt(mainBalanceString);
+
+
+  // remaining balance 
+
+  const remainingBalance = mainBalanceValue - totalSavingsInIncome;
+
+  
+
+  // get remaining balance
+
+  const remainingBalanceElement = document.getElementById('remaining-balance');
+  const remainingBalanceElementString = remainingBalanceElement.innerText;
+  const remainingBalanceElementValue = parseInt(remainingBalanceElementString);
+
+  const totalRemainingBalance = remainingBalanceElementValue + remainingBalance;
+
+  remainingBalanceElement.innerText = totalRemainingBalance;
+
+})
