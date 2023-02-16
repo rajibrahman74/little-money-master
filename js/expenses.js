eb46f6464327fabdd301ad7a961ab1111e010bfb
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-16 14:36:27 +0600

    [ADD]-functionality-and-validation

--- js/expenses.js
+++ js/expenses.js
@@ -1,1 +1,94 @@
-// 
+// Add event listener in the calculate button
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+
+
+  // get Income field
+  const incomeField = document.getElementById("income-field");
+  const incomeFieldString = incomeField.value;
+  const incomeFieldValue = parseInt(incomeFieldString);
+
+  // validate income input
+  if (
+    isNaN(incomeFieldValue) ||
+    incomeFieldValue <= 0 ||
+    incomeFieldString.trim() === ""
+  ) {
+    alert("Please enter a valid positive number for income");
+    incomeField.value = "";
+    return;
+  }
+
+
+
+
+  // get food field
+  const foodField = document.getElementById("food-field");
+  const foodFieldString = foodField.value;
+  const foodFieldValue = parseInt(foodFieldString);
+
+  // validate food input
+  if (
+    isNaN(foodFieldValue) ||
+    foodFieldValue < 0 ||
+    foodFieldString.trim() === ""
+  ) {
+    alert("Please enter a valid positive number or zero for food expenses");
+    foodField.value = "";
+    return;
+  }
+
+
+
+  // get rent field
+  const rentField = document.getElementById("rent-field");
+  const rentFieldString = rentField.value;
+  const rentFieldValue = parseInt(rentFieldString);
+
+  // validate rent input
+  if (
+    isNaN(rentFieldValue) ||
+    rentFieldValue < 0 ||
+    rentFieldString.trim() === ""
+  ) {
+    alert("Please enter a valid positive number or zero for rent expenses");
+    rentField.value = "";
+    return;
+  }
+
+
+
+  // get clothes field
+  const clothesField = document.getElementById("clothes-field");
+  const clothesFieldString = clothesField.value;
+  const clothesFieldValue = parseInt(clothesFieldString);
+
+  // validate clothes input
+  if (
+    isNaN(clothesFieldValue) ||
+    clothesFieldValue < 0 ||
+    clothesFieldString.trim() === ""
+  ) {
+    alert("Please enter a valid positive number or zero for clothing expenses");
+    clothesField.value = "";
+    return;
+  }
+
+
+
+  // Expenses element
+  const expnessesTotal = document.getElementById("expenses-total");
+  const expnessesTotalString = expnessesTotal.innerText;
+  const expnessesTotalValue = parseInt(expnessesTotalString);
+
+
+  // Total expenses calculation
+  const totalExpenses = foodFieldValue + rentFieldValue + clothesFieldValue;
+  const totalExpensesValue = totalExpenses + expnessesTotalValue;
+
+  expnessesTotal.innerText = totalExpensesValue;
+
+
+
+  
+});
