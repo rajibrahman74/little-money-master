e209c4761467bc09f5f47a64ae2e0181f65a77b7
rajibrahman74e <rajiburrahman74@gmail.com>
2023-02-17 07:47:53 +0600

    [FIX]

---
+++ js/calculation.js
@@ -0,0 +1,95 @@
+// Expensses total and main balance calculate button addd event handller
+
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const incomeField = getInputValue("income-field");
+
+  const foodField = getInputValue("food-field");
+
+  const rentField = getInputValue("rent-field");
+
+  const clothesField = getInputValue("clothes-field");
+
+  // validation for all here input fields together
+
+  if (
+    isNaN(incomeField) ||
+    isNaN(foodField) ||
+    isNaN(rentField) ||
+    isNaN(clothesField) ||
+    incomeField < 0 ||
+    foodField < 0 ||
+    rentField < 0 ||
+    clothesField < 0
+  ) {
+    alert("Please enter valid input for all fields");
+    return;
+  }
+
+  // get total expensses
+
+  const totalExpenses = foodField + rentField + clothesField;
+
+  // validation,if total expensse getter then income
+
+  if (totalExpenses > incomeField) {
+    alert("You don't have enough income");
+    return;
+  }
+
+  const totalExpensesElement = document.getElementById("expenses-total");
+  totalExpensesElement.innerText = totalExpenses;
+
+  // main balance
+
+  const mainBalanceElement = document.getElementById("main-balance");
+  const mainBalanceTotal = incomeField - totalExpenses;
+  mainBalanceElement.innerText = mainBalanceTotal;
+});
+
+
+
+
+// save button add event handller
+
+document.getElementById("save-btn").addEventListener("click", function () {
+  const incomeField = getInputValue("income-field");
+
+  const savingField = getInputValue("saving-field");
+
+  // Validation for income field and saving field
+
+  if (
+    isNaN(incomeField) ||
+    isNaN(savingField) ||
+    incomeField < 0 ||
+    savingField < 0
+  ) {
+    alert("Please enter valid input for all fields");
+    return;
+  }
+
+  // savings calculation
+
+  const totalSaving = incomeField * (savingField / 100);
+
+  if (savingField > 100) {
+    alert("You Don't save getter then 100%");
+    return;
+  }
+
+  // total savings in income
+
+  const totalSavingElement = document.getElementById("total-savings");
+  totalSavingElement.innerText = totalSaving;
+
+  const remainingBalance = document.getElementById("remaining-balance");
+
+  const totalExpenses = getTextElementValue("expenses-total");
+
+  const totalSavings = getTextElementValue("total-savings");
+
+  const totalCost = totalExpenses + totalSavings;
+
+  const remainingTotal = incomeField - totalCost;
+  remainingBalance.innerText = remainingTotal;
+});
